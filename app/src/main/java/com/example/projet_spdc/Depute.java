568e0471e4e2fcf1972156e884809fbc4bdd793c
Kaputchino <andreasmulard@gmail.com>
2024-02-26 15:13:58 +0100

    classe groupe et depute

---
+++ app/src/main/java/com/example/projet_spdc/Depute.java
@@ -0,0 +1,15 @@
+package com.example.projet_spdc;
+
+import java.util.ArrayList;
+
+public class Depute {
+    static ArrayList<Depute> listDepute;
+    int id;
+
+    @Override
+    public String toString() {
+        return "Depute{" +
+                "id=" + id +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/projet_spdc/Groupe.java
@@ -0,0 +1,17 @@
+package com.example.projet_spdc;
+
+import android.graphics.Color;
+
+import java.util.ArrayList;
+
+public class Groupe {
+    static ArrayList<Groupe> listeGroupe;
+    int id;
+    String slug;
+    String nom;
+    String acronyme;
+    Boolean isActive;
+    Color color;
+    int order;
+    String link;
+}
