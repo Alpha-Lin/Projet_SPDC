0f93d28e2927b910b3ef77585dfa7136f87e3d42
Kaputchino <andreasmulard@gmail.com>
2024-03-09 15:03:56 +0100

    something doesn't work

--- app/src/main/java/com/example/projet_spdc/MainActivity.java
+++ app/src/main/java/com/example/projet_spdc/MainActivity.java
@@ -17,9 +17,12 @@ public class MainActivity extends AppCompatActivity {
         for(Groupe g : Groupe.listeGroupe){
             Log.w("jkhfjkd",g.getNom());
         };
-
+/**
         Intent intent_MP = new Intent(this, MP_Activity.class);
-        startActivity(intent_MP);
+        startActivity(intent_MP);**/
+        Intent group_activity = new Intent(this, GroupeActivity.class);
+        group_activity.putExtra("groupe",2);
+        startActivity(group_activity);
 
     }
 }
