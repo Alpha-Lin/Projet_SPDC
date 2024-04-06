e2afb8538ba9627fc67070831aabbeff9b87328f
Kaputchino <andreasmulard@gmail.com>
2024-04-06 20:37:00 +0200

    on clean

--- app/src/main/java/com/example/projet_spdc/MainActivity.java
+++ app/src/main/java/com/example/projet_spdc/MainActivity.java
@@ -4,14 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
